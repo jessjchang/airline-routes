cdb65c96c61940f6500b6dcf748f784c288d3124
Jessica Chang <jesschang77@gmail.com>
2022-04-17 01:44:37 -0400

    Display raw data for routes from data.js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+View app at https://airline-routes-jc.herokuapp.com/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data';
 
 const App = () => (
   <div className="app">
@@ -7,9 +8,24 @@ const App = () => (
     <h1 className="title">Airline Routes</h1>
   </header>
   <section>
-    <p>
-      Welcome to the app!
-    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>airline</th>
+          <th>src</th>
+          <th>dest</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.routes.map(route => (
+          <tr key={route.airline+route.src+route.dest}>
+            <td>{route.airline}</td>
+            <td>{route.src}</td>
+            <td>{route.dest}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </section>
 </div>
 )
